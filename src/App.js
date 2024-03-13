0f65eb5bb43c9fb2de906231d20690107048fde2
KirillAstafev <gamerstyle.ya.ru@gmail.com>
2024-03-14 00:33:23 +0300

    Стартовая страница

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import LandingPage from "./starting-page/LandingPage";
 
 function App() {
     return (
         <div className="App">
-
+            <LandingPage/>
         </div>
     );
 }
