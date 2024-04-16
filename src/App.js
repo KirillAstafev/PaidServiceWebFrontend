c9eb598caa4bfe41ad21d333df4e4f279c8fa2e9
KirillAstafev <gamerstyle.ya.ru@gmail.com>
2024-04-16 21:48:13 +0300

    Добавление перехода по ссылкам (React Router Link)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import LandingPage from "./starting-page/LandingPage";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import MainContent from "./starting-page/components/routes/main/MainContent";
+import PricesTable from "./starting-page/components/routes/prices/PricesTable";
 
 const router = createBrowserRouter(
     [
@@ -12,6 +13,10 @@ const router = createBrowserRouter(
                 {
                     index: true,
                     element: <MainContent/>
+                },
+                {
+                    path: "/prices",
+                    element: <PricesTable/>
                 }
             ]
         }
--- src/starting-page/components/AppAppBar.tsx
+++ src/starting-page/components/AppAppBar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { PaletteMode } from '@mui/material';
+import {PaletteMode} from '@mui/material';
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -11,187 +11,191 @@ import MenuItem from '@mui/material/MenuItem';
 import Drawer from '@mui/material/Drawer';
 import MenuIcon from '@mui/icons-material/Menu';
 import ToggleColorMode from './ToggleColorMode';
+import {NavLink} from "react-router-dom";
 
 interface AppAppBarProps {
-  mode: PaletteMode;
-  toggleColorMode: () => void;
+    mode: PaletteMode;
+    toggleColorMode: () => void;
 }
 
-function AppAppBar({ mode, toggleColorMode }: AppAppBarProps) {
-  const [open, setOpen] = React.useState(false);
+function AppAppBar({mode, toggleColorMode}: AppAppBarProps) {
+    const [open, setOpen] = React.useState(false);
 
-  const toggleDrawer = (newOpen: boolean) => () => {
-    setOpen(newOpen);
-  };
+    const toggleDrawer = (newOpen: boolean) => () => {
+        setOpen(newOpen);
+    };
 
-  const scrollToSection = (sectionId: string) => {
-    const sectionElement = document.getElementById(sectionId);
-    const offset = 128;
-    if (sectionElement) {
-      const targetScroll = sectionElement.offsetTop - offset;
-      sectionElement.scrollIntoView({ behavior: 'smooth' });
-      window.scrollTo({
-        top: targetScroll,
-        behavior: 'smooth',
-      });
-      setOpen(false);
-    }
-  };
+    const scrollToSection = (sectionId: string) => {
+        const sectionElement = document.getElementById(sectionId);
+        const offset = 128;
+        if (sectionElement) {
+            const targetScroll = sectionElement.offsetTop - offset;
+            sectionElement.scrollIntoView({behavior: 'smooth'});
+            window.scrollTo({
+                top: targetScroll,
+                behavior: 'smooth',
+            });
+            setOpen(false);
+        }
+    };
 
-  return (
-    <div>
-      <AppBar
-        position="fixed"
-        sx={{
-          boxShadow: 0,
-          bgcolor: 'transparent',
-          backgroundImage: 'none',
-          mt: 2,
-        }}
-      >
-        <Container maxWidth="lg">
-          <Toolbar
-            variant="regular"
-            sx={(theme) => ({
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              flexShrink: 0,
-              borderRadius: '999px',
-              bgcolor:
-                theme.palette.mode === 'light'
-                  ? 'rgba(255, 255, 255, 0.4)'
-                  : 'rgba(0, 0, 0, 0.4)',
-              backdropFilter: 'blur(24px)',
-              maxHeight: 40,
-              border: '1px solid',
-              borderColor: 'divider',
-              boxShadow:
-                theme.palette.mode === 'light'
-                  ? `0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)`
-                  : '0 0 1px rgba(2, 31, 59, 0.7), 1px 1.5px 2px -1px rgba(2, 31, 59, 0.65), 4px 4px 12px -2.5px rgba(2, 31, 59, 0.65)',
-            })}
-          >
-            <Box
-              sx={{
-                flexGrow: 1,
-                display: 'flex',
-                alignItems: 'center',
-                ml: '-18px',
-                px: 0,
-              }}
+    return (
+        <div>
+            <AppBar
+                position="fixed"
+                sx={{
+                    boxShadow: 0,
+                    bgcolor: 'transparent',
+                    backgroundImage: 'none',
+                    mt: 2,
+                }}
             >
-              <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                  <MenuItem
-                      onClick={() => scrollToSection('features')}
-                      sx={{ py: '6px', px: '12px' }}
-                  >
-                      <Typography variant="body2" color="text.primary">
-                          Запись на приём
-                      </Typography>
-                  </MenuItem>
-                  <MenuItem
-                      onClick={() => scrollToSection('testimonials')}
-                      sx={{ py: '6px', px: '12px' }}
-                  >
-                      <Typography variant="body2" color="text.primary">
-                          Прейскурант платных услуг
-                      </Typography>
-                  </MenuItem>
-                  <MenuItem
-                      onClick={() => scrollToSection('highlights')}
-                      sx={{ py: '6px', px: '12px' }}
-                  >
-                      <Typography variant="body2" color="text.primary">
-                          Нормативные документы
-                      </Typography>
-                  </MenuItem>
-              </Box>
-            </Box>
-            <Box
-              sx={{
-                display: { xs: 'none', md: 'flex' },
-                gap: 0.5,
-                alignItems: 'center',
-              }}
-            >
-              <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode} />
-            </Box>
-            <Box sx={{ display: { sm: '', md: 'none' } }}>
-              <Button
-                variant="text"
-                color="primary"
-                aria-label="menu"
-                onClick={toggleDrawer(true)}
-                sx={{ minWidth: '30px', p: '4px' }}
-              >
-                <MenuIcon />
-              </Button>
-              <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
-                <Box
-                  sx={{
-                    minWidth: '60dvw',
-                    p: 2,
-                    backgroundColor: 'background.paper',
-                    flexGrow: 1,
-                  }}
-                >
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      alignItems: 'end',
-                      flexGrow: 1,
-                    }}
-                  >
-                    <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode} />
-                  </Box>
-                  <MenuItem onClick={() => scrollToSection('features')}>
-                    Features
-                  </MenuItem>
-                  <MenuItem onClick={() => scrollToSection('testimonials')}>
-                    Testimonials
-                  </MenuItem>
-                  <MenuItem onClick={() => scrollToSection('highlights')}>
-                    Highlights
-                  </MenuItem>
-                  <MenuItem onClick={() => scrollToSection('pricing')}>
-                    Pricing
-                  </MenuItem>
-                  <MenuItem onClick={() => scrollToSection('faq')}>FAQ</MenuItem>
-                  <Divider />
-                  <MenuItem>
-                    <Button
-                      color="primary"
-                      variant="contained"
-                      component="a"
-                      href="/material-ui/getting-started/templates/sign-up/"
-                      target="_blank"
-                      sx={{ width: '100%' }}
+                <Container maxWidth="lg">
+                    <Toolbar
+                        variant="regular"
+                        sx={(theme) => ({
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                            flexShrink: 0,
+                            borderRadius: '999px',
+                            bgcolor:
+                                theme.palette.mode === 'light'
+                                    ? 'rgba(255, 255, 255, 0.4)'
+                                    : 'rgba(0, 0, 0, 0.4)',
+                            backdropFilter: 'blur(24px)',
+                            maxHeight: 40,
+                            border: '1px solid',
+                            borderColor: 'divider',
+                            boxShadow:
+                                theme.palette.mode === 'light'
+                                    ? `0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)`
+                                    : '0 0 1px rgba(2, 31, 59, 0.7), 1px 1.5px 2px -1px rgba(2, 31, 59, 0.65), 4px 4px 12px -2.5px rgba(2, 31, 59, 0.65)',
+                        })}
                     >
-                      Sign up
-                    </Button>
-                  </MenuItem>
-                  <MenuItem>
-                    <Button
-                      color="primary"
-                      variant="outlined"
-                      component="a"
-                      href="/material-ui/getting-started/templates/sign-in/"
-                      target="_blank"
-                      sx={{ width: '100%' }}
-                    >
-                      Sign in
-                    </Button>
-                  </MenuItem>
-                </Box>
-              </Drawer>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </div>
-  );
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: 'flex',
+                                alignItems: 'center',
+                                ml: '-18px',
+                                px: 0,
+                            }}
+                        >
+                            <Box sx={{display: {xs: 'none', md: 'flex'}}}>
+                                <MenuItem sx={{py: '6px', px: '12px'}}>
+                                    <NavLink to={"/"}>
+                                        <Typography variant="body2" color="text.primary">
+                                            На главную
+                                        </Typography>
+                                    </NavLink>
+                                </MenuItem>
+                                <MenuItem sx={{py: '6px', px: '12px'}}>
+                                    <Typography variant="body2" color="text.primary">
+                                        Запись на приём
+                                    </Typography>
+                                </MenuItem>
+                                <MenuItem sx={{py: '6px', px: '12px'}}>
+                                    <NavLink to={"/prices"}>
+                                        <Typography variant="body2" color="text.primary">
+                                            Прейскурант платных услуг
+                                        </Typography>
+                                    </NavLink>
+                                </MenuItem>
+                                <MenuItem
+                                    onClick={() => scrollToSection('highlights')}
+                                    sx={{py: '6px', px: '12px'}}
+                                >
+                                    <Typography variant="body2" color="text.primary">
+                                        Нормативные документы
+                                    </Typography>
+                                </MenuItem>
+                            </Box>
+                        </Box>
+                        <Box
+                            sx={{
+                                display: {xs: 'none', md: 'flex'},
+                                gap: 0.5,
+                                alignItems: 'center',
+                            }}
+                        >
+                            <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode}/>
+                        </Box>
+                        <Box sx={{display: {sm: '', md: 'none'}}}>
+                            <Button
+                                variant="text"
+                                color="primary"
+                                aria-label="menu"
+                                onClick={toggleDrawer(true)}
+                                sx={{minWidth: '30px', p: '4px'}}
+                            >
+                                <MenuIcon/>
+                            </Button>
+                            <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
+                                <Box
+                                    sx={{
+                                        minWidth: '60dvw',
+                                        p: 2,
+                                        backgroundColor: 'background.paper',
+                                        flexGrow: 1,
+                                    }}
+                                >
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            alignItems: 'end',
+                                            flexGrow: 1,
+                                        }}
+                                    >
+                                        <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode}/>
+                                    </Box>
+                                    <MenuItem onClick={() => scrollToSection('features')}>
+                                        Features
+                                    </MenuItem>
+                                    <MenuItem onClick={() => scrollToSection('testimonials')}>
+                                        Testimonials
+                                    </MenuItem>
+                                    <MenuItem onClick={() => scrollToSection('highlights')}>
+                                        Highlights
+                                    </MenuItem>
+                                    <MenuItem onClick={() => scrollToSection('pricing')}>
+                                        Pricing
+                                    </MenuItem>
+                                    <MenuItem onClick={() => scrollToSection('faq')}>FAQ</MenuItem>
+                                    <Divider/>
+                                    <MenuItem>
+                                        <Button
+                                            color="primary"
+                                            variant="contained"
+                                            component="a"
+                                            href="/material-ui/getting-started/templates/sign-up/"
+                                            target="_blank"
+                                            sx={{width: '100%'}}
+                                        >
+                                            Sign up
+                                        </Button>
+                                    </MenuItem>
+                                    <MenuItem>
+                                        <Button
+                                            color="primary"
+                                            variant="outlined"
+                                            component="a"
+                                            href="/material-ui/getting-started/templates/sign-in/"
+                                            target="_blank"
+                                            sx={{width: '100%'}}
+                                        >
+                                            Sign in
+                                        </Button>
+                                    </MenuItem>
+                                </Box>
+                            </Drawer>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </div>
+    );
 }
 
 export default AppAppBar;
